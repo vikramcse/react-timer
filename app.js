aed7591a71290b83f813a046d4ad25839c54e08d
vikramcse <vikramcse.10@gmail.com>
2015-11-25 22:05:59 +0530

    Updated the basic timer to Stopwatch

--- app.js
+++ app.js
@@ -1,29 +1,61 @@
+function getFormattedSeconds(sec) {
+	var seconds = ('0' + sec % 60).slice(-2);
+	var minuites = Math.floor(sec / 60);
+	return minuites + ':' + seconds;
+}
+
 var Timer = React.createClass({
 	getInitialState: function() {
-		return {
-			elapsed: 0
-		};
+		return {elapsed: 0, laps: []};
+	},
+	getSeconds: function() {
+		return ('0' + this.state.elapsed % 60 ).slice(-2);
+	},
+	getMinuits: function() {
+		return Math.floor(this.state.elapsed / 60);
 	},
 	tick: function() {
-		this.setState({elapsed: new Date() - this.props.start})
+		this.setState({elapsed: this.state.elapsed + 1});
 	},
-	componentDidMount: function() {
-		this.interval = setInterval(this.tick, 50);
+	handleStartClick: function() {
+		this.interval = setInterval(this.tick, 1000);
 	},
-	componentWillUnmount: function() {
-    	clearInterval(this.interval);
-  	},
+	handleStopClick: function() {
+		clearInterval(this.interval);
+		this.setState({lastClearedInterval: this.interval});
+	},
+	handleReset: function() {
+		this.setState({elapsed: 0, laps: []});
+	},
+	handleLaps: function() {
+		this.setState({laps: this.state.laps.concat([this.state.elapsed])});
+	},
 	render: function() {
-		var elapsed = Math.round(this.state.elapsed / 100);
-		var seconds = (elapsed / 10).toFixed(1);
-
 		return(
-			<div>
-				<p>This example was started <b>{seconds} seconds</b> ago.</p>
-				{this.props.start}
+			<div className="stopwatch">
+				<h1 className="stopwatch-timer">{getFormattedSeconds(this.state.elapsed)}</h1>
+
+				{(this.state.elapsed === 0 || this.interval === this.state.lastClearedInterval)
+					? <button className="btn start-btn" onClick={this.handleStartClick}>start</button>
+					: <button className="btn stop-btn" onClick={this.handleStopClick}>stop</button>
+				}
+
+				{(this.state.elapsed !== 0 && this.interval !== this.state.lastClearedInterval)
+					? <button className="btn"  onClick={this.handleLaps}>lap</button>
+					: null
+				}
+
+				{(this.state.elapsed !== 0 && this.interval === this.state.lastClearedInterval)
+					? <button className="btn"  onClick={this.handleReset}>reset</button>
+					: null
+				}
+
+				<ul className="stopwatch-laps">{this.state.laps.map(function (lap, i) {
+					return <li className="stopwatch-lap"><strong>{i + 1}</strong> / {getFormattedSeconds(lap)}</li>
+				})}</ul>
 			</div>
 		);
 	}
 });
 
-ReactDOM.render(<Timer start={Date.now()}/>, document.getElementById('content'));
+ReactDOM.render(<Timer/>, document.getElementById('content'));
